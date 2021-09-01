f3472fd0a614d6951b575297944eab64bd16b21f
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2021-09-01 21:37:02 +0200

    feat(i18n): add datetime formats for cs and en-US

--- src/i18n.ts
+++ src/i18n.ts
@@ -13,6 +13,40 @@ const i18n = createI18n({
   legacy: false,
   fallbackLocale: "en-US",
   messages,
+  datetimeFormats: {
+    "en-US": {
+      time: {},
+      date: {},
+      datetime: {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      },
+    },
+    cs: {
+      time: {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      },
+      date: {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      },
+      datetime: {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      },
+    },
+  },
 });
 
 export default i18n;
