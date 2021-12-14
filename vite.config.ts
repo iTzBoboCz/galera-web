c27f3591b1ec16395a8a4024f86971e04a4cfd96
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2021-12-14 17:33:51 +0100

    feat: add environment variable BACKEND_URL for connecting to backend API

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/": {
-        target: "http://127.0.0.1:8000/",
+        target: process.env.BACKEND_URL ?? "http://127.0.0.1:8000/",
         rewrite: (path) => path.replace(/^\/api/, ""),
         changeOrigin: true,
       },
