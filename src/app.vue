0795934ae4e49fa1e4fff55d070ec6a35da67081
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2021-12-20 23:15:59 +0100

    refactor: change the structure of layouts and fix navigation drawer

--- src/app.vue
+++ src/app.vue
@@ -1,22 +1,14 @@
 <template>
-  <div id="galera">
-    <v-app :theme="darkMode ? 'dark' : 'light'">
-      <NavigationMenu />
-      <router-view />
-    </v-app>
-    <MediaModal v-if="isModalActive" />
-  </div>
+  <v-app :theme="darkMode ? 'dark' : 'light'">
+    <router-view />
+  </v-app>
 </template>
 
 <script lang="ts">
 import { storeToRefs } from "pinia";
 import { defineComponent } from "vue";
 
-import MediaModal from "~/components/media-modal.vue";
-import { useSelectedMediaStore } from "~/stores/selected-media";
 import { useUserPreferencesStore } from "~/stores/user-preferences";
-
-import NavigationMenu from "./components/navigation-menu.vue";
 
 // TODO: use only script setup when this issue is solved:
 // https://github.com/import-js/eslint-plugin-import/issues/2243
@@ -28,22 +20,4 @@ export default defineComponent({
 <script setup lang="ts">
 const userPreferences = useUserPreferencesStore();
 const { darkMode } = storeToRefs(userPreferences);
-
-const selectedMedia = useSelectedMediaStore();
-const { isModalActive } = storeToRefs(selectedMedia);
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-#logo {
-  margin: 0 auto;
-}
-</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,30 +1,18 @@
-<script lang="ts">
-import { RouteRecordNormalized } from "vue-router";
-
-import router from "~/router";
-
-export default {
-  data(): { routes: RouteRecordNormalized[] } {
-    return {
-      routes: router.getRoutes(),
-    };
-  },
-};
-</script>
-
 <template>
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <div class="w-1/4 m-auto text-center text-gray-300 bg-teal-800">
-      Default Layout
-    </div>
+  <NavigationMenu />
+  <v-main>
     <router-view />
-    <div class="links">
-      <p>Other pages:</p>
-      <div v-for="route in routes" :key="route.path">
-        <div v-if="route.children.length > 0">
-          <router-link :to="route.path">{{ route.path }}</router-link>
-        </div>
-      </div>
-    </div>
-  </main>
+  </v-main>
+  <MediaModal v-if="isModalActive" />
 </template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+
+import MediaModal from "~/components/media-modal.vue";
+import NavigationMenu from "~/components/navigation-menu.vue";
+import { useSelectedMediaStore } from "~/stores/selected-media";
+
+const selectedMedia = useSelectedMediaStore();
+const { isModalActive } = storeToRefs(selectedMedia);
+</script>
