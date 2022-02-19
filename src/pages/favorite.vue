183534bfe270d52f46576538b74e4ed41adc0ca4
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2022-02-19 13:46:01 +0100

    fix(favorite): load photos properly when reloading

--- src/pages/favorite.vue
+++ src/pages/favorite.vue
@@ -1,17 +1,30 @@
 <template>
   <v-container>
-    <ViewWrapper :media-list="likedMedia ?? []" />
+    <ViewWrapper v-if="loaded" :media-list="likedMedia ?? []" />
   </v-container>
 </template>
 
 <script setup lang="ts">
+import { MediaResponse } from "@galera/client-axios";
+import { ref } from "vue";
+
 import ViewWrapper from "~/components/view-wrapper.vue";
 import { useFetchedMediaStore } from "~/stores/fetched-media";
 
 const fetchedMedia = useFetchedMediaStore();
 
-fetchedMedia.getLikedMedia();
+const loaded = ref(false);
 
 // we need to cache this so the liked images remain there even after unliking
-const likedMedia = fetchedMedia.likedMedia;
+const likedMedia: MediaResponse[] = [];
+
+// eslint-disable-next-line promise/catch-or-return
+fetchedMedia.getLikedMedia().then(() => {
+  if (fetchedMedia.likedMedia) {
+    likedMedia.push(...fetchedMedia.likedMedia);
+    loaded.value = true;
+  }
+
+  return;
+});
 </script>
