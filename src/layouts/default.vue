b678d6d4631fa964eb825477a84db32613f5775e
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2022-03-01 23:07:25 +0100

    fix(default-layout): hide advanced navigation when not logged in

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
-  <NavigationMenu />
+  <!-- TODO: remake this later -->
+  <NavigationMenu v-if="auth.isLoggedIn" />
+  <v-toolbar v-else>
+    <v-spacer />
+    <ViewChanger />
+  </v-toolbar>
   <v-main>
     <router-view />
   </v-main>
@@ -9,10 +14,13 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
 
+import ViewChanger from "~/components/buttons/view-changer.vue";
 import MediaModal from "~/components/media-modal.vue";
 import NavigationMenu from "~/components/navigation-menu.vue";
+import { useAuthStore } from "~/stores/auth";
 import { useSelectedMediaStore } from "~/stores/selected-media";
 
+const auth = useAuthStore();
 const selectedMedia = useSelectedMediaStore();
 const { isModalActive } = storeToRefs(selectedMedia);
 </script>
