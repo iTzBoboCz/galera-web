6653a6a8a00ce02bd565fbd6c60b49db841b93f9
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2022-03-06 23:57:50 +0100

    fix(api): try to refresh only BearerAuth token

--- src/composables/api.ts
+++ src/composables/api.ts
@@ -55,57 +55,66 @@ export function defaultConfiguration(
 }
 
 export default function api(config?: Configuration): DefaultApi {
-  const auth = useAuthStore();
-
-  const { isLoggedIn } = storeToRefs(auth);
-
   const finalConfiguration = config ? config : defaultConfiguration();
 
   const axiosInstance = axios.create();
 
-  axiosInstance.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    async (error: AxiosError) => {
-      // refresh token if server responds with Unauthorized
-      if (error.response?.status == 401) {
-        await auth.refreshToken();
+  // Tries to refresh user's token when request fails
+  // used only for BearerAuth
+  if (
+    finalConfiguration.baseOptions &&
+    finalConfiguration.baseOptions.headers &&
+    finalConfiguration.baseOptions.headers.Authorization &&
+    finalConfiguration.baseOptions.headers.Authorization.startsWith("Bearer")
+  ) {
+    const auth = useAuthStore();
 
-        // if there are no headers, we can't send a request with certainty because the server could only accept some content-type, etc.
-        if (isLoggedIn.value && error.config.headers) {
-          // retry last request
-          const headers = error.config.headers;
-          headers.Authorization = `Bearer ${auth.bearerToken?.bearerTokenEncoded}`;
+    const { isLoggedIn } = storeToRefs(auth);
 
-          // eslint-disable-next-line promise/no-promise-in-callback
-          const retry = await axios({
-            method: error.config.method,
-            url: error.config.url,
-            data: error.config.data,
-            headers,
-            responseType: error.config.responseType,
-          })
-            .then((response) => {
-              // must be response, not response.data
-              return response;
+    axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      async (error: AxiosError) => {
+        // refresh token if server responds with Unauthorized
+        if (error.response?.status == 401) {
+          await auth.refreshToken();
+
+          // if there are no headers, we can't send a request with certainty because the server could only accept some content-type, etc.
+          if (isLoggedIn.value && error.config.headers) {
+            // retry last request
+            const headers = error.config.headers;
+            headers.Authorization = `Bearer ${auth.bearerToken?.bearerTokenEncoded}`;
+
+            // eslint-disable-next-line promise/no-promise-in-callback
+            const retry = await axios({
+              method: error.config.method,
+              url: error.config.url,
+              data: error.config.data,
+              headers,
+              responseType: error.config.responseType,
             })
-            .catch(() => {
-              return;
-            });
+              .then((response) => {
+                // must be response, not response.data
+                return response;
+              })
+              .catch(() => {
+                return;
+              });
 
-          if (retry) {
-            return retry;
+            if (retry) {
+              return retry;
+            }
           }
+
+          await auth.logOut();
+          router.go(0);
+
+          return Promise.reject(error);
         }
-
-        await auth.logOut();
-        router.go(0);
-
-        return Promise.reject(error);
       }
-    }
-  );
+    );
+  }
 
   return new DefaultApi(
     finalConfiguration,
