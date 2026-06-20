aa4d913f65f64d0849249724936cec952250b50a
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2026-06-20 15:39:59 +0200

    fix(deps): switch to vite-plugin-vue-layouts fork and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import VueI18n from "@intlify/unplugin-vue-i18n/vite";
 import Vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import Pages from "vite-plugin-pages";
-import Layouts from "vite-plugin-vue-layouts";
+import Layouts from "vite-plugin-vue-layouts-next";
 
 // https://vitejs.dev/config/
 export default defineConfig({
