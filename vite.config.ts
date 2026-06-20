4487f29675750a44d3b6f21cf73b21dd85327ad9
Ondřej Pešek <iTzBoboCz@users.noreply.github.com>
2026-06-20 15:08:26 +0200

    chore: modernise vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import path from "node:path";
+import process from "node:process";
 import VueI18n from "@intlify/unplugin-vue-i18n/vite";
 import Vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
@@ -20,7 +21,7 @@ export default defineConfig({
     alias: {
       // FIX: fix this later
       // eslint-disable-next-line unicorn/prefer-module
-      "~/": `${path.resolve(__dirname, "src")}/`,
+      "~/": `${path.resolve(import.meta.dirname, "src")}/`,
     },
   },
   plugins: [
@@ -28,7 +29,7 @@ export default defineConfig({
     VueI18n({
       // FIX: fix this later
       // eslint-disable-next-line unicorn/prefer-module
-      include: path.resolve(__dirname, "./src/locales/**"),
+      include: path.resolve(import.meta.dirname, "./src/locales/**"),
     }),
     Pages(),
     Layouts(),
